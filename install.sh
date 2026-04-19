032f9e9e8b6a2d16ec34a18dc9563f7550b8f8fc
Ronit Nagarapu <ronitnag04@gmail.com>
2026-04-19 15:12:33 -0700

    Updated install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-env="source /opt/aws_neuronx_venv_pytorch_2_5/bin/activate"
+env="source /opt/aws_neuronx_venv_pytorch_2_9/bin/activate"
 name=$(hostname)
 
 if ! grep -q "$env" ~/.bashrc; then
@@ -10,11 +10,11 @@ fi
 source ~/.bashrc
 
 if [ ! -f influxdata-archive_compat.key ]; then
-    wget -q https://repos.influxdata.com/influxdata-archive_compat.key
-    echo '393e8779c89ac8d958f81f942f9ad7fb82a25e133faddaf92e15b16e6ac9ce4c influxdata-archive_compat.key' | sha256sum -c && cat influxdata-archive_compat.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg > /dev/null
-    echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive_compat.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
+    wget -q https://repos.influxdata.com/influxdata-archive.key
+    gpg --no-default-keyring --show-keys --with-fingerprint --with-colons ./influxdata-archive.key 2>&1 | grep -q '^fpr:\+24C975CBA61A024EE1B631787C3D57159FC2F927:$' && cat influxdata-archive.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdata-archive.gpg > /dev/null
+    echo 'deb [signed-by=/etc/apt/trusted.gpg.d/influxdata-archive.gpg] https://repos.influxdata.com/debian stable main' | sudo tee /etc/apt/sources.list.d/influxdata.list
 
-    sudo apt-get update && sudo apt-get install influxdb2 influxdb2-cli -y
+    sudo apt-get update && sudo apt-get install influxctl influxdb2 influxdb2-cli -y
     sudo systemctl start influxdb
 
     influx setup \
